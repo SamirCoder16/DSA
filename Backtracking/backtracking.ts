c9752a87ec1c2449a1aa8ab0dd6e4e8b0ea2fbf9
SamirCoder16 <samirparvez48@gmail.com>
2026-08-17 09:37:14 -0700

    typescript error commited

--- Backtracking/backtracking.ts
+++ Backtracking/backtracking.ts
@@ -1,3 +1,15 @@
+
+interface NodeStatic {
+    name: string;
+    age: number
+}
+
+class Node: NodeStatic {
+    constructor (name: string, age : number){
+            
+    }
+}
+
 function Backtracking () : void {
 
 }
